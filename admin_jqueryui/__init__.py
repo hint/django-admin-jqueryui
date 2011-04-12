f476a5c375b61eccbc247083e48e0422dbf47c2e
centralniak <p.kilczuk@stylesheet.pl>
2011-04-12 13:01:19 +0200

    PEP8 fixes

--- admin_jqueryui/__init__.py
+++ admin_jqueryui/__init__.py
@@ -2,6 +2,7 @@ from django.contrib.admin.options import ModelAdmin
 
 old_media = ModelAdmin._media
 
+
 def new_media(self):
     media_instance = old_media(self)
     media_instance.add_js(('admin_jqueryui/js/admin_jqueryui.min.js',))
