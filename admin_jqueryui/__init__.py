b7eaeef1ea668f2965613e902e21016b8b2eec88
Daniel Boczek <daniel.boczek@gmail.com>
2014-03-31 00:05:47 +0200

    Django 1.4 compatibility

--- admin_jqueryui/__init__.py
+++ admin_jqueryui/__init__.py
@@ -1,10 +1,10 @@
 from django.contrib.admin.options import ModelAdmin
 
-old_media = ModelAdmin._media
+ModelAdmin.old_media = ModelAdmin.media
 
 
 def new_media(self):
-    media_instance = old_media(self)
+    media_instance = self.old_media
     media_instance.add_js(('admin_jqueryui/js/admin_jqueryui.min.js',))
     return media_instance
 ModelAdmin.media = property(new_media)
